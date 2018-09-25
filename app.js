684fea46d51d8672c0ed79790523cb572a468294
sungwlee1 <43590432+sungwlee1@users.noreply.github.com>
2018-09-25 19:42:03 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+ $.ajax({
+  url:'emails.json',
+  dataType:'json',
+  type: 'get',
+  cache: false,
+  success: function(data){
+    console.log(data);
+  }
+
+ });
+
+
+
+// $.getJSON("emails.json", function(data){
+//   $.each(data, function(key, val){
+//         tr = $('<tr>');
+//         tr.append("<td>" + (value.sender) + "</td>");
+//         tr.append("<td>" + (value.subject) + "</td>");
+//         tr.append("<td>" + (value.tags) + "</td>");
+//         tr.append("<td>" + (value.date) + "</td>");
+//         $('#emailBody').append(tr);
+//   });
+// });
+
+});
+
+
