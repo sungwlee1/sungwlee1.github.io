30eac30087af0f3772244ff690f52f6619bb5de5
sungwlee1 <43590432+sungwlee1@users.noreply.github.com>
2018-09-25 21:16:57 -0400

    testing

--- app.js
+++ app.js
@@ -8,44 +8,56 @@ $(document).ready(function() {
   success: function(data){
     $(data.messages).each(function(index, value){
         tr = $('<tr>');
-        tr.append("<td>" + (value.sender) + "</td>");
-        tr.append("<td>" + (value.subject) + "</td>");
-        tr.append("<td>" + (value.tags) + "</td>");
-        tr.append("<td>" + (value.date) + "</td>");
+        tr.append("<td data-toggle="modal" data-id="index" data-target="#emailModal">" + (value.sender) + "</td>");
+        tr.append("<td data-toggle="modal" data-id="index" data-target="#emailModal">" + (value.subject) + "</td>");
+        tr.append("<td data-toggle="modal" data-id="index" data-target="#emailModal">" + (value.tags) + "</td>");
+        tr.append("<td data-toggle="modal" data-id="index" data-target="#emailModal">" + (value.date) + "</td>");
         $('#emailBody').append(tr);
     });
 
     //Making sure ajax was successful
-    console.log(data);
 
 
   }
 
  });
 
+$(function(){
+    $('#orderModal').modal({
+        keyboard: true,
+        backdrop: "static",
+        show:false,
+        
+    }).on('show', function(){
+          var getIdFromRow = $(event.target).closest('tr').data('id');
+        //make your ajax call populate items or what even you need
+        $(this).find('#orderDetails').html($('<b> Order Id selected: ' + getIdFromRow  + '</b>'))
+    });
+    
+});
 //create collection of emails
 // let arr1 = generateEmails();
 
-  if (arr1) {
-    // clone that collection into a second array
-    let arr2 = [...arr1];
-    // For each member of the email arr2 collection, create a table row.
-    $.each(arr2, function(index, element) {
-      // createEmailRow() creates the DOM email row.
-      let myRow = createEmailRow(index, element);
-      // Stick that row we just created into the email table.
-      $('#emailBody').append(myRow);
-      // Add that student into the actual school info.
-      populateEmailInfo(index, element);
-    });
-  }
+//   if (arr1) {
+//     // clone that collection into a second array
+//     let arr2 = [...arr1];
+//     // For each member of the email arr2 collection, create a table row.
+//     $.each(arr2, function(index, element) {
+//       // createEmailRow() creates the DOM email row.
+//       let myRow = createEmailRow(index, element);
+//       // Stick that row we just created into the email table.
+//       $('#emailBody').append(myRow);
+//       // Add that student into the actual school info.
+//       populateEmailInfo(index, element);
+//     });
+//   }
 
-  $('#myModal').on('show.bs.modal', function(e) {
-    var idx = $(e.relatedTarget).closest('tr').index();
-    $('#emailModalBody tr').hide().filter('[emailIndex=' + idx + ']').show();
-  });
+//   $('#myModal').on('show.bs.modal', function(e) {
+//     var idx = $(e.relatedTarget).closest('tr').index();
+//     $('#emailModalBody tr').hide().filter('[emailIndex=' + idx + ']').show();
+//   });
 
-});
+// });
 
 // function createEmailRow(index, obj) {
 //   // console.log(obj);
