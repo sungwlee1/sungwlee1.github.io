1baf28df49c965c87c9da5437f33a621182147b1
sungwlee1 <43590432+sungwlee1@users.noreply.github.com>
2018-09-25 20:27:28 -0400

    Modal try#1
    Trying to do a modal with table first.

--- app.js
+++ app.js
@@ -1,35 +1,74 @@
 $(document).ready(function() {
+
+//create collection of emails
+let arr1 = generateEmails();
+
+  if (arr1) {
+    // clone that collection into a second array
+    let arr2 = [...arr1];
+    // For each member of the email arr2 collection, create a table row.
+    $.each(arr2, function(index, element) {
+      // createEmailRow() creates the DOM email row.
+      let myRow = createEmailRow(index, element);
+      // Stick that row we just created into the email table.
+      $('#emailBody').append(myRow);
+      // Add that student into the actual school info.
+      populateEmailInfo(index, element);
+    });
+  }
+
+  $('#myModal').on('show.bs.modal', function(e) {
+    var idx = $(e.relatedTarget).closest('tr').index();
+    $('#schoolModalBody tr').hide().filter('[emailIndex=' + idx + ']').show();
+  });
+  
+});
+
+function generateEmails() {
  $.ajax({
   url:'emails.json',
   dataType:'json',
   type: 'get',
   cache: false,
   success: function(data){
-    $(data.messages).each(function(index, value){
-        tr = $('<tr>');
-        tr.append("<td>" + (value.sender) + "</td>");
-        tr.append("<td>" + (value.subject) + "</td>");
-        tr.append("<td>" + (value.tags) + "</td>");
-        tr.append("<td>" + (value.date) + "</td>");
-        $('#emailBody').append(tr);
-    });
+    // $(data.messages).each(function(index, value){
+    //     tr = $('<tr>');
+    //     tr.append("<td>" + (value.sender) + "</td>");
+    //     tr.append("<td>" + (value.subject) + "</td>");
+    //     tr.append("<td>" + (value.tags) + "</td>");
+    //     tr.append("<td>" + (value.date) + "</td>");
+    //     $('#emailBody').append(tr);
+    // });
+
+    //Making sure ajax was successful
+    console.log(data);
   }
 
  });
+}
 
+function createEmailRow(index, obj) {
+  // console.log(obj);
+  tr = $('<tr class="email-row">');
+  tr.append("<td>" + (obj.sender || "") + "</td>");
+  tr.append("<td>" + (obj.subject || "") + "</td>");
+  tr.append("<td>" + (obj.tags || "") + "</td>");
+  tr.append("<td>" + (obj.date || "") + "</td>");
 
+  return tr;
+}
 
-// $.getJSON("emails.json", function(data){
-//   $.each(data, function(key, val){
-//         tr = $('<tr>');
-//         tr.append("<td>" + (value.sender) + "</td>");
-//         tr.append("<td>" + (value.subject) + "</td>");
-//         tr.append("<td>" + (value.tags) + "</td>");
-//         tr.append("<td>" + (value.date) + "</td>");
-//         $('#emailBody').append(tr);
-//   });
-// });
+function populateEmailInfo(index, obj) {
+  var tr;
+  $.each(obj, function(i, value) {
+    tr = $('<tr>', {
+      emailIndex: index
+    });
+      tr.append("<td>" + "<button id='modalBtn" + index + "' type='button' class='btn btn-info' data-toggle='modal' data-target='#myModal'>Info</button>"
+    + "</td>");
+    tr.append("<td>" + (value.body || "") + "</td>");
+    $('#schoolModalBody').append(tr);
 
-});
-
+  });
+}
 
